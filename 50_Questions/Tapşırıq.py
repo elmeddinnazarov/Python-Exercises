3506d705ac2d25c495969fa9c9b2f800fb20ad68
elmeddinnazarov <elmeddin.nazarov0@gmail.com>
2022-08-13 22:30:58 +0400

    50 exercies

---
+++ 50_Questions/Tapşırıq.py
@@ -0,0 +1,68 @@
+################################################
+#################### Sual 1 ####################
+
+# for i in range(1500, 2700):
+#     if not i%7 and not i%5:
+#         print(i, end=", - ")
+
+###############################################
+################### Sual 2 ####################
+
+# print("This program convert celsius to fahrenheit and fahrenheit to celsius.\nwhich service do you want?\n\n")
+# while True:
+#     inp=input("\nEnter '1' to convert celsius to fahrenheit,\nEnter '2' to convert fahrenheit to celsius.")
+#     if not inp.isnumeric():
+#         print("\nJust numbers please.")
+#     elif inp=="1":
+#         c=input("Enter Celsius: ")
+#         if not c.isnumeric():
+#             print("\nJust numbers please.")
+#         else:
+#             f=(int(c)*1.8)+32
+#             print(str(int(f)))
+#             break
+#     elif inp=="2":
+#         f=input("Enter Celsius: ")
+#         if not f.isnumeric():
+#             print("\nJust numbers please.")
+#         else:
+#             c=(int(f)-32)/1.8
+#             print(str(int(c)))
+#             break
+
+###############################################
+################### Sual 3 ####################
+
+
+
+
+
+
+###############################################
+################### Sual 6 ####################
+
+# numbers = [1,2,3,4,5,6,7,8,9,10,11]
+# cüt =[]
+# tek =[]
+
+# for i in numbers:
+#     if not i%2:
+#         cüt.append(i)
+#     elif i%2:
+#         tek.append(i)
+
+# print(len(cüt), len(tek))
+
+        
+        
+        
+
+    
+        
+    
+        
+        
+
+
+
+
